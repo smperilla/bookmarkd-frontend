bd20ce126fbefbe3c869423105ee05bcd7ed71a0
ryland <ryland@MacBook-Pro-4.lan1>
2023-09-26 13:54:24 -0700

    added alphabetical sort feature

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,11 +13,31 @@ const SearchBar = (props) => {
         })
         props.setBookmarks(shownBookmarks)
     }
+    const alphaClick = (e)=>{
+        e.preventDefault()
+        const bookmarksCopy = [...props.allBookmarks]
+        const alphaBookmarks=bookmarksCopy.sort((a,b)=>
+        a.title.localeCompare(b.title))
+        props.setBookmarks(alphaBookmarks)
+    }
+    const chronoClick = (e)=>{
+        e.preventDefault()
+        const bookmarksCopy = [...props.allBookmarks]
+        props.setBookmarks(bookmarksCopy)
+    }
   return (
     <div>
         <form ref={formRef} style={{textAlign:'center'}} onChange={handleChange}>
             <input className='add-bookmark-input' type="text" name='searchterm' placeholder='Search for a Bookmark'/>
         </form>
+        <div style={{display:'flex', justifyContent:'space-around', marginTop: '20px'}}>
+            <form onSubmit={alphaClick}>
+                <input className='add-bookmark-btn' type="submit" value="Alphabetical Order"/>
+            </form>
+            <form onSubmit={chronoClick}>
+                <input className='add-bookmark-btn' type="submit" value="Chronological Order"/>
+            </form>
+        </div>
     </div>
   )
 }
